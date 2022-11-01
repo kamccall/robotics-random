90a8a91b48a265521128841312a6380b169442c7
k mccall <kmccall@microsoft.com>
2022-11-01 14:02:15 -0700

    Update kalman_filter_1d.cpp

--- kalman_filter_1d.cpp
+++ kalman_filter_1d.cpp
@@ -66,5 +66,26 @@ int main()
       printf("predict: [%f, %f]\n", mu, sig);
     }
     */
+    
+    // another great tuple example: look like useful std::ignore is
+    /*
+    int main () 
+    {
+      int myint;
+      char mychar;
+
+      std::tuple<int,float,char> mytuple;
+
+      mytuple = std::make_tuple(10, 2.6, 'a');
+
+      std::tie(myint, std::ignore, mychar) = mytuple;
+
+      std::cout << "myint contains: " << myint << '\n';
+      std::cout << "mychar contains: " << mychar << '\n';
+
+      return 0;
+    }
+    */
+    
     return 0;
 }
